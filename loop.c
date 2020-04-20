b305ef272df2d9c2a035defecabddcc9624ded8a
Miroslav Pavlak <mpavlak3@gmail.com>
2020-04-20 21:59:12 +0200

    Pipe redrect in/out

--- loop.c
+++ loop.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
 #include <readline/readline.h>
 #include <readline/history.h>
 #include "func.h"
@@ -75,26 +79,153 @@ int exec_cmd(char * command, int argc)
 	return 1;
 }
 
-exec_cmd_
-
-int execute_commands(char ** command, int argc)
+int exec_pipes(char ** command, int argc)
 {
-	int ret_val;
-	if(argc < 1)
-			return 0;
-	else if(argc == 1){
-		if(check_builtin(command[0])){
-			ret_val = execute_builtin(command[0]);
-			return ret_val;
-		}
-		else{
-			ret_val = exec_cmd(command[0], argc);	
-		}
-	}
-	else if(argc > 1){
+        int pipe_fd[2], status, argc_num, counter = 0;
+        pid_t child1, child2;
+        printf("%s %s %s %d\n", command[0], command[1], command[2], argc);
+        if(pipe(pipe_fd) < 0){
+                perror("pipe()\n");
+                return 0;
+        }
+                do{
+                        if((child1 = fork()) == 0){
+                                        char ** cmd = (char **)malloc(MAX_COMMANDS_LEN*sizeof(char*));
+                                        cmd = pars_args(command[0], " ", &argc_num);
+                                        dup2(pipe_fd[0], STDIN_FILENO);
+                                        dup2(pipe_fd[1], STDOUT_FILENO);
+                                        close(pipe_fd[0]);
+                                        close(pipe_fd[1]);
+                                        if(execvp(cmd[counter], cmd) < 0)
+                                                        perror("execvp()");
+                                        _Exit(EXIT_FAILURE);
+                        }
+                        else if(child1 < 0){
+                                        perror("fork()\n");
+                                        return 0;
+                        }
+                        else{
+                                        waitpid(child1, &status, WUNTRACED);
+                                        counter++;
+                        }
+                }while(counter < argc-1);
 
-	}
-	return 1;
+                 if((child2 = fork()) == 0){
+                                char ** cmd2 = (char **)malloc(MAX_COMMANDS_LEN*sizeof(char*));
+                                cmd2 = pars_args(command[1], " ", &argc_num);
+                                dup2(pipe_fd[0], STDIN_FILENO);
+                                close(pipe_fd[1]);
+                                close(pipe_fd[0]);
+                                if(execvp(cmd2[0], cmd2) < 0)
+                                        perror("execvp()");
+                                _Exit(EXIT_FAILURE);
+                        }
+                        else if(child2 < 0){
+                                        perror("fork()");
+                                        return 0;
+                        }
+                        else{
+                                        waitpid(child2, &status, WNOHANG);
+                        }
+
+        close(pipe_fd[0]);
+        close(pipe_fd[1]);
+
+        return 1;
+}
+
+int exec_redirect_inout(char ** command, int argc, int in_out)
+{
+        int fd, status, argc_num;
+        pid_t child;
+        char *file_name = malloc(20*sizeof(char));
+        if(strlen(command[1]) > 19){
+                error("Too long name of file");
+                return 0;
+        }
+        if((file_name = get_file_name(command[1])) == NULL)
+                return  0;
+
+        if(in_out == 0){
+                if((fd = open(file_name, O_RDONLY, S_IRUSR | S_IWUSR)) < 0){
+                        perror("open()");
+                        return 0;
+                }
+        }
+        else if (in_out == 1){
+                if((fd = open(file_name, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR)) < 0){
+                        perror("open()");
+                        return 0;
+                }
+        }
+        else
+                return 0;
+
+        if((child = fork()) == 0){
+                char ** cmd = (char **)malloc(MAX_COMMANDS_LEN*sizeof(char*));
+                cmd = pars_args(command[0], " ", &argc_num);
+                close(in_out);
+                dup(fd);
+                if(execvp(cmd[0], cmd) < 0)
+                       perror("execvp()\n");
+                _Exit(EXIT_FAILURE);
+        }
+        else if(child < 0){
+                perror("fork()\n");
+                return 0;
+        }
+        else{
+                waitpid(child, &status, WUNTRACED);
+        }
+        close(fd);
+
+        return 1;
+}
+
+int execute_commands(char ** command, char * cmd, int argc)
+{
+        int ret_val;
+        if(argc < 1)
+                return 0;
+        else if(argc == 1){
+                if(check_builtin(command[0])){
+                        ret_val = execute_builtin(command[0]);
+                        return ret_val;
+                }
+                else{
+                        ret_val = exec_cmd(command[0], argc);
+                }
+        }
+        else if(argc == 2){
+                if(redirect_output(cmd) && !found_pipes(cmd) && !redirect_input(cmd)){
+                        printf("a > b\n");
+                        ret_val = exec_redirect_inout(command, argc, 1);
+                }
+                //<
+                else if(redirect_input(cmd) && !found_pipes(cmd) && !redirect_output(cmd)){
+                        printf("a < b\n");
+                        ret_val = exec_redirect_inout(command, argc, 0);
+                }
+                //||>
+                else if(redirect_output(cmd) && found_pipes(cmd) && !redirect_input(cmd) && pipe_redir_out(cmd)){
+                        printf("a | b > c\n");
+
+                }
+                //|||
+                else if(found_pipes(cmd) && !redirect_output(cmd) && !redirect_input(cmd)){
+                        printf("a | b | c\n");
+                        ret_val = exec_pipes(command, argc);
+                }
+                else{
+                        printf("Ambiguous output/input redirect.\n");
+                        return 0;
+                }
+        }
+        else{
+                error("Invalid number of Arguments");
+                return 0;
+        }
+        return ret_val;
 }
 
 void shell_loop()
